fb12a390e5bc708488b203e87bf82a2dc0e1f21f
mbocianski <miles.a.bocianski@gmail.com>
2022-12-07 15:40:27 -0800

    reverted to previous version

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json'); 
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000; 
+const cors = require("cors");
 
 server.use(middlewares);
 server.use(router);
+server.use(cors())
 
 server.listen(port);
